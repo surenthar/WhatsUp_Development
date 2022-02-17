01dda151e0bdadff0c46ed793e2cba2a4c78e8ef
surenthar <43981225+surenthar@users.noreply.github.com>
2022-02-17 20:50:44 +0530

    Changes Updated

--- Demo.java
+++ Demo.java
@@ -2,5 +2,6 @@ public class Demo{
  public static void main(String[] args){
 	System.out.println("Testing the program");
 	System.out.println("Testing whatsup");
+	 System.out.println("Testing whatsup");
 	}
 }
