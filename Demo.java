de306acf45bfafd40a2264211f05d0345a330ad8
surenthar <43981225+surenthar@users.noreply.github.com>
2022-02-17 20:57:47 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,6 +2,5 @@ public class Demo{
  public static void main(String[] args){
 	System.out.println("Testing the program");
 	System.out.println("Testing whatsup");
-	 System.out.println("Testing whatsup");
 	}
 }
